768aba331bfb2617fa09bfd914d63d57311b471f
JLdgu <Jonathan.Linstead@devon.gov.uk>
2023-05-03 13:01:29 +0100

    Convert Sites table

--- PeripheralAudit/Program.cs
+++ PeripheralAudit/Program.cs
@@ -1,10 +1,23 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using PeripheralAudit.Application;
 
-DbContextOptionsBuilder builder = new();
-builder.UseSqlite("data source=peripheralaudit.db;");
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(ConfigureDatabase)
+.Build();
 
-PeripheralAuditDbContext dbContext = new(builder.Options);
+PeripheralAuditDbContext dbContext = host.Services.GetRequiredService<PeripheralAuditDbContext>();
+
 //dbContext.Database.EnsureDeleted();
 dbContext.Database.EnsureCreated();
 //dbContext.Database.Migrate();
+
+void ConfigureDatabase(HostBuilderContext context, IServiceCollection services)
+{
+    string? connectionString = context.Configuration.GetConnectionString("Default");
+
+    services.AddDbContext<PeripheralAuditDbContext>(
+                options => options.UseSqlite(connectionString));
+}
---
+++ convert/convert.ps1
@@ -0,0 +1,5 @@
+<#
+c:\Users\passp\Development\SQLiteTools\sqlite3.exe -init .\convert.sqlite3
+#>
+d:\SQLiteTools\sqlite3.exe -init convert.sqlite3
+.quit 1
