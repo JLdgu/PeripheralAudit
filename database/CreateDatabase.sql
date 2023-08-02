44503f004731ef9bb21bc88141f5b5173532a412
JLdgu <jonathan.linstead@devon.gov.uk>
2023-08-02 11:17:47 +0100

    add location update trigger change lastupdate default

--- database/CreateDatabase.sql
+++ database/CreateDatabase.sql
@@ -2,27 +2,7 @@ CREATE TABLE IF NOT EXISTS "Site" (
     "Id" INTEGER NOT NULL CONSTRAINT "PK_Site" PRIMARY KEY AUTOINCREMENT,
     "Name" TEXT NOT NULL
 );
-
-CREATE TABLE IF NOT EXISTS "Location" (
-    "Id" INTEGER NOT NULL CONSTRAINT "PK_Location" PRIMARY KEY AUTOINCREMENT,
-    "Name" TEXT NOT NULL,
-    "SiteId" INTEGER NOT NULL,
-    "DeskCount" INTEGER NOT NULL,
-    "MonitorSingleCount" INTEGER NOT NULL,
-    "MonitorDualCount" INTEGER NOT NULL,
-    "MonitorGradeBronzeCount" INTEGER NOT NULL,
-    "MonitorGradeSilverCount" INTEGER NOT NULL,
-    "MonitorGradeGoldCount" INTEGER NOT NULL,
-    "DockCount" INTEGER NOT NULL,
-    "DockPsuCount" INTEGER NOT NULL,
-    "PcCount" INTEGER NOT NULL,
-    "KeyboardCount" INTEGER NOT NULL,
-    "MouseCount" INTEGER NOT NULL,
-    "ChairCount" INTEGER NULL,
-    "Note" TEXT NULL, LastUpdate TEXT NOT NULL,
-    CONSTRAINT "FK_Location_Site_SiteId" FOREIGN KEY ("SiteId") REFERENCES "Site" ("Id") ON DELETE CASCADE
-);
-
+CREATE TABLE sqlite_sequence(name,seq);
 CREATE TABLE IF NOT EXISTS "Dock" (
     "Id" INTEGER NOT NULL CONSTRAINT "PK_Dock" PRIMARY KEY AUTOINCREMENT,
     "LocationId" INTEGER NOT NULL,
@@ -32,7 +12,6 @@ CREATE TABLE IF NOT EXISTS "Dock" (
     "Model" TEXT NOT NULL,
     CONSTRAINT "FK_Dock_Location_LocationId" FOREIGN KEY ("LocationId") REFERENCES "Location" ("Id") ON DELETE CASCADE
 );
-
 CREATE TABLE IF NOT EXISTS "Monitor" (
     "Id" INTEGER NOT NULL CONSTRAINT "PK_Monitor" PRIMARY KEY AUTOINCREMENT,
     "Size" INTEGER NOT NULL,
@@ -43,7 +22,36 @@ CREATE TABLE IF NOT EXISTS "Monitor" (
     "Model" TEXT NOT NULL,
     CONSTRAINT "FK_Monitor_Location_LocationId" FOREIGN KEY ("LocationId") REFERENCES "Location" ("Id") ON DELETE CASCADE
 );
-
 CREATE INDEX "IX_Dock_LocationId" ON "Dock" ("LocationId");
-CREATE INDEX "IX_Location_SiteId" ON "Location" ("SiteId");
 CREATE INDEX "IX_Monitor_LocationId" ON "Monitor" ("LocationId");
+CREATE TABLE IF NOT EXISTS "Location" (
+	"Id"	INTEGER NOT NULL,
+	"Name"	TEXT NOT NULL,
+	"SiteId"	INTEGER NOT NULL,
+	"DeskCount"	INTEGER NOT NULL,
+	"MonitorSingleCount"	INTEGER NOT NULL,
+	"MonitorDualCount"	INTEGER NOT NULL,
+	"MonitorGradeBronzeCount"	INTEGER NOT NULL,
+	"MonitorGradeSilverCount"	INTEGER NOT NULL,
+	"MonitorGradeGoldCount"	INTEGER NOT NULL,
+	"DockCount"	INTEGER NOT NULL,
+	"DockPsuCount"	INTEGER NOT NULL,
+	"PcCount"	INTEGER NOT NULL,
+	"KeyboardCount"	INTEGER NOT NULL,
+	"MouseCount"	INTEGER NOT NULL,
+	"ChairCount"	INTEGER,
+	"Note"	TEXT,
+	"LastUpdate"	TEXT NOT NULL DEFAULT CURRENT_DATE,
+	CONSTRAINT "PK_Location" PRIMARY KEY("Id" AUTOINCREMENT),
+	CONSTRAINT "FK_Location_Site_SiteId" FOREIGN KEY("SiteId") REFERENCES "Site"("Id") ON DELETE CASCADE
+);
+CREATE INDEX "IX_Location_SiteId" ON "Location" (
+	"SiteId"
+);
+CREATE TRIGGER Location_Update
+    AFTER UPDATE ON Location
+    FOR EACH ROW
+    WHEN NEW.LastUpdate = OLD.LastUpdate    
+BEGIN
+   UPDATE Location SET LastUpdate = CURRENT_DATE WHERE Id = old.Id;
+END;
